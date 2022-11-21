a36c71431384fd962b57c7dc607cbaf15c1844ad
Bob-cafezento <successstudent9@gmail.com>
2022-11-21 09:21:37 -0300

    Criando conta, e erro 500 solucionado

--- src/views/HistoriaView.vue
+++ src/views/HistoriaView.vue
@@ -1,21 +1,21 @@
 <template>
-        <h1>
-        SUCCESS STUDENT
-    </h1>
-    <div id="abrangent">
-        <main>
-            <div class="box" @click="$router.push('/antiguidade')">
-                Antiguidade
-            </div>
-            <div class="box">
-                Idade Média
-            </div>
-            <div class="box">
-                Era Moderna
-            </div>
-            <div class="box">
-                Era Comtempôranea
-            </div>
-        </main>
-    </div>
+  <h1>
+    SUCCESS STUDENT
+  </h1>
+  <div id="abrangent">
+    <main>
+      <div class="box" @click="$router.push('/antiguidade')">
+        Antiguidade
+      </div>
+      <div class="box">
+        Idade Média
+      </div>
+      <div class="box">
+        Era Moderna
+      </div>
+      <div class="box">
+        Era Comtempôranea
+      </div>
+    </main>
+  </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,36 +1,34 @@
-<script>
-
-</script>
+<script></script>
 
 <template>
-    <!--Aqui nós temos o resto do conteúdo-->
-    <h1>
-        SUCCESS STUDENT
-    </h1>
-    <div id="abrangent">
-        <main>
-            <div class="box" @click="$router.push('/linguagens')">
-                <img src="@/assets/imagens/Linguagens.png">
-                Português
-            </div>
-            <div class="box" @click="$router.push('/matematica')">
-                <img src="@/assets/imagens/Esquadro.png">
-                Matemática
-            </div>
-            <div class="box" @click="$router.push('/historia')">   
-                <img src="@/assets/imagens/Ampulheta.png"><br>
-                História
-            </div>
-            <div class="box" @click="$router.push('/geografia')">
-                <img src="@/assets/imagens/geografia.png" >
-                Geografia
-            </div>
-        </main>
-    </div>
+  <!--Aqui nós temos o resto do conteúdo-->
+  <h1>
+    SUCCESS STUDENT
+  </h1>
+  <div id="abrangent">
+    <main>
+      <div class="box" @click="$router.push('/linguagens')">
+        <img src="@/assets/imagens/Linguagens.png" />
+        Português
+      </div>
+      <div class="box" @click="$router.push('/matematica')">
+        <img src="@/assets/imagens/Esquadro.png" />
+        Matemática
+      </div>
+      <div class="box" @click="$router.push('/historia')">
+        <img src="@/assets/imagens/Ampulheta.png" /><br />
+        História
+      </div>
+      <div class="box" @click="$router.push('/geografia')">
+        <img src="@/assets/imagens/geografia.png" />
+        Geografia
+      </div>
+    </main>
+  </div>
 </template>
 
 <style scoped>
 .box img {
-    height: 200px;
+  height: 200px;
 }
 </style>
